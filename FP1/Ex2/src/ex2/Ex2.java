bc88d458d6dcf78a4c3f3ebe8d47fa623aec3fa9
NERD-X <nerdxontheline@outlook.com>
2019-10-04 13:45:47 +0100

    Ex2 Done

---
+++ FP1/Ex2/src/ex2/Ex2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex2;
+
+/**
+ *
+ * @author NERD-X
+ */
+public class Ex2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Pair<String> tmp = new Pair("Hello", "World");
+        System.out.println(tmp.max());
+        
+    }
+    
+}
---
+++ FP1/Ex2/src/ex2/Pair.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex2;
+
+/**
+ *
+ * @author NERD-X
+ */
+public class Pair<T extends Comparable> {
+
+    private T first;
+    private T second;
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setSecond(T second) {
+        this.second = second;
+    }
+
+    
+    
+    public T max() {
+        if (first.compareTo(second) >= 0) {
+            return first;
+        } else {
+            return second;
+        }
+    }
+}

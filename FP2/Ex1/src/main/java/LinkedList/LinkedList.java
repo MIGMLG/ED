a612dfb3351d496f9ec4575ac748c3a78f3aef23
NERD-X <nerdxontheline@outlook.com>
2019-10-13 16:05:00 +0100

    FP2 Ex1 Done

---
+++ FP2/Ex1/src/main/java/LinkedList/LinkedList.java
@@ -0,0 +1,77 @@
+package LinkedList;
+
+public class LinkedList<T> {
+
+    private LinkNode<T> head, tail;
+    private long size;
+
+    public LinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public boolean add(T data){
+        if(head == null){
+            LinkNode<T> current = new LinkNode<T>(data, null);
+            this.head = current;
+            this.tail = current;
+            this.size++;
+            return true;
+        }else{
+            LinkNode<T> current = this.tail;
+            LinkNode<T> next = new LinkNode<>(data, null);
+            current.setNext(next);
+            this.tail = next;
+            this.size++;
+            return true;
+        }
+
+    }
+
+    public T remove(T data) throws LinkedListExceptions {
+        if(this.head == null){
+            throw new LinkedListExceptions(LinkedListExceptions.EMPTYLIST);
+        }
+        else{
+            LinkNode<T> current = head;
+            LinkNode<T> previous = null;
+
+            boolean found = false;
+            while(current!=null && found == false){
+                if(current.getData().equals(data)){
+                    found = true;
+                }else{
+                    previous = current;
+                    current = current.getNext();
+                }
+            }
+
+            if(found == true){
+                if(this.size == 1){
+                    this.head = null;
+                    this.tail = null;
+                }else if(current.getData().equals(head.getData())){
+                    head = head.getNext();
+                }else if(current.getData().equals(tail.getData())){
+                    previous.setNext(null);
+                    tail = previous;
+                }else {
+                    previous.setNext(current.getNext());
+                }
+                this.size--;
+                return current.getData();
+            }else {
+                throw new LinkedListExceptions(LinkedListExceptions.NOTFOUND);
+            }
+        }
+    }
+
+    public void printList(){
+        LinkNode<T> current = head;
+        while(current!=null){
+            System.out.println(current.toString());
+            current= current.getNext();
+        }
+    }
+}

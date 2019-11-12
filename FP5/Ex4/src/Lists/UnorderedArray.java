d27e3dc708996ecb74c3c9773e46e92be41fd940
DevMig <nerdxontheline@outlook.com>
2019-11-12 19:58:02 +0000

    Fix some Bugs

--- FP5/Ex4/src/Lists/UnorderedArray.java
+++ FP5/Ex4/src/Lists/UnorderedArray.java
@@ -4,17 +4,21 @@ public class UnorderedArray<T> extends ArrayList<T> implements UnorderedListADT<
 
     @Override
     public void addToFront(T element) {
-        if(rear == list.length){
-            expandCapacity();
+        if (isEmpty()) {
+            list[0] = element;
+        } else {
+            if (rear == list.length) {
+                expandCapacity();
+            }
+
+            int first = 0;
+            for (int i = rear - 1; i >= first; i--) {
+
+                list[i + 1] = list[i];
+
+            }
+            list[first] = element;
         }
-
-        int first = 0;
-        for (int i = rear - 1; i >= first; i--) {
-
-            list[i + 1] = list[i];
-
-        }
-        list[first] = element;
         rear++;
         size++;
         modCount++;
@@ -22,11 +26,15 @@ public class UnorderedArray<T> extends ArrayList<T> implements UnorderedListADT<
 
     @Override
     public void addToRear(T element) {
-        if(rear == list.length){
-            expandCapacity();
-        }
+        if (isEmpty()) {
+            list[0] = element;
+        } else {
+            if (rear == list.length) {
+                expandCapacity();
+            }
 
-        list[rear] = element;
+            list[rear] = element;
+        }
         rear++;
         size++;
         modCount++;
@@ -34,31 +42,35 @@ public class UnorderedArray<T> extends ArrayList<T> implements UnorderedListADT<
 
     @Override
     public void addAfter(T element, T target) throws ListExceptions {
-        if(rear == list.length){
-            expandCapacity();
-        }
+        if (!isEmpty()) {
+            if (rear == list.length) {
+                expandCapacity();
+            }
 
-        boolean found = false;
-        int current = 0;
+            boolean found = false;
+            int current = 0;
 
-        while (current < size && found == false) {
-            if (list[current].equals(target)) {
-                found = true;
+            while (current < size && found == false) {
+                if (list[current].equals(target)) {
+                    found = true;
+                } else {
+                    current++;
+                }
+            }
+
+            if (found == true) {
+                for (int i = rear - 1; i > current; i--) {
+                    list[i + 1] = list[i];
+                }
+                list[current + 1] = element;
+                rear++;
+                size++;
+                modCount++;
             } else {
-                current++;
+                throw new ListExceptions(ListExceptions.ELEMENT_NOT_FOUND);
             }
-        }
-
-        if (found == true) {
-            for (int i = rear - 1; i > current; i--) {
-                list[i + 1] = list[i];
-            }
-            list[current + 1] = element;
-            rear++;
-            size++;
-            modCount++;
         } else {
-            throw new ListExceptions(ListExceptions.ELEMENT_NOT_FOUND);
+            throw new ListExceptions(ListExceptions.EMPTY_LIST);
         }
     }
 

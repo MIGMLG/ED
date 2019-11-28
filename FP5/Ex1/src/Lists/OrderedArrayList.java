ecd795eb0df6305a80f70519e5e865f59115800d
MIGUEL ANGELO MARTINS DA COSTA <8160525@estg.ipp.pt>
2019-11-28 16:32:20 +0000

    Optimizing add

--- FP5/Ex1/src/Lists/OrderedArrayList.java
+++ FP5/Ex1/src/Lists/OrderedArrayList.java
@@ -8,25 +8,31 @@ public class OrderedArrayList<T> extends ArrayList<T> implements OrderedListADT<
             if (isEmpty()) {
                 list[0] = element;
             } else {
-                boolean found = false;
-                int current = 0;
 
-                while (current < size() && found == false) {
-                    Comparable<T> tmp = (Comparable<T>) list[current];
-                    if (tmp.compareTo(element) > 0) {
-                        found = true;
-                    } else {
-                        current++;
-                    }
-                }
+                Comparable rearPosition = (Comparable) list[rear - 1];
 
-                if (found == true) {
-                    for (int i = rear - 1; i >= current; i--) {
-                        list[i + 1] = list[i];
-                    }
-                    list[current] = element;
-                } else {
+                if (rearPosition.compareTo(element) < 0) {
                     list[rear] = element;
+                } else {
+
+                    boolean found = false;
+                    int current = 0;
+
+                    while (current < size() && found == false) {
+                        Comparable<T> tmp = (Comparable<T>) list[current];
+                        if (tmp.compareTo(element) > 0) {
+                            found = true;
+                        } else {
+                            current++;
+                        }
+                    }
+
+                    if (found == true) {
+                        for (int i = rear - 1; i >= current; i--) {
+                            list[i + 1] = list[i];
+                        }
+                        list[current] = element;
+                    }
                 }
             }
 
